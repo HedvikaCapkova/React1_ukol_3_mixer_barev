5c97cc6054997b292dc9e23eb5a402e09e9b8bea
HedvikaCapkova <hedvika.capkova@gmail.com>
2023-03-20 20:30:40 +0100

    added UseState

--- src/ColorSlider/ColorSlider.jsx
+++ src/ColorSlider/ColorSlider.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import './ColorSlider.css';
 
-export const ColorSlider = () => {
+export const ColorSlider = ({ baseColor, colorName }) => {
   return (
-    <div class="sliders">
-      <label for="red">Červená</label>
+    <div className="sliders">
+      <label htmlFor={baseColor}>{colorName}</label>
       <input
         type="range"
-        class="slider slider--red"
-        id="redSlider"
+        className={`slider slider--${baseColor}`}
+        id={`${baseColor}Slider`}
         min="0"
         max="255"
         value="0"
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ColorSlider } from './ColorSlider/ColorSlider';
 import './style.css';
 
 const App = () => {
+  const [redValue, setRedValue] = useState(0);
+  const [greenValue, setGreenValue] = useState(0);
+  const [blueValue, setBlueValue] = useState(0);
+
   return (
-    <div className="container">
-      <ColorSlider />
+    <div className="color-panel">
+      <h1>Mixér barev</h1>
+      <ColorSlider
+        baseColor="red"
+        colorName="Červená"
+      />
+      <ColorSlider
+        baseColor="green"
+        colorName="Zelená"
+      />
+      <ColorSlider
+        baseColor="blue"
+        colorName="Modná"
+      />
+      <div
+        className="color-box"
+        id="color-box"></div>
     </div>
   );
 };
